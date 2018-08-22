f1a364d5e78b32525e2cfb1f776b86456634798b
tuomas tuominen <tuom457@outlook.com>
2018-08-22 14:18:46 +0300

    infowindowit lisätty

---
+++ src/components/InfoWindowMap.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+
+
+class InfoWindowMap extends Component {
+    constructor(props){
+        super(props);
+    
+        this.state = {
+            isOpen: false
+        }
+    
+    }
+    
+    handleToggleOpen = () => {
+    
+        this.setState({
+            isOpen: true
+        });
+    }
+    
+    handleToggleClose = () => {
+        this.setState({
+            isOpen: false
+        });
+    }
+  render() {
+    return (
+        <Marker
+        key={this.props.index}
+        position={{ lat: parseFloat(this.props.lat), lng: parseFloat(this.props.lng)}}
+        label={this.props.toilet_id}
+        onClick={() => this.handleToggleOpen()}
+    >
+
+    {
+        this.state.isOpen &&
+     <InfoWindow onCloseClick={this.handleToggleClose}>
+         <h1>kukkuu!!!</h1>
+     </InfoWindow>
+     }
+
+
+    </Marker>
+
+)
+    
+  }
+}
+
+export default InfoWindowMap;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
-import {getAllToilets} from '../utilities/Service'
-
-
+import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import { getAllToilets } from '../utilities/Service'
+import "../Map.css"
+import InfoWindowMap from './InfoWindowMap.js'
 class Map extends Component {
   constructor() {
     super()
@@ -10,45 +10,97 @@ class Map extends Component {
       lat: 60.17131,
       lng: 24.94145,
       zoom: 17,
-      markers: []
+      markers: [],
+      setlatlng: [],
+      info: false
     }
   }
-
-  componentDidMount(){
+  
+  componentDidMount() {
+   
     var allToilets = []
     getAllToilets((data) => {
       data.map(res => {
-       allToilets.push(res)
+        allToilets.push(res)
       })
-      
-      this.setState({markers: allToilets})
+
+      this.setState({ markers: allToilets })
     });
   }
-   
-   render() {
-    const googleMarkers = this.state.markers.map(marker => (
-      <Marker
-          position={{ lat: parseFloat(marker.latitude), lng: parseFloat(marker.longitude) } }
-          key={marker.toilet_id}
-        />
-    ));
-   const Map = withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter = { { lat: 60.171944, lng: 24.941389 } }
-        defaultZoom = { 16 }
-      >
-      {googleMarkers}
+  getLocation = (event) => {
+    this.state.setlatlng = [event.latLng.lat(), event.latLng.lng()]
+    console.log(this.state.setlatlng)
+  }
+
+  addToilet = () => {
+    document.getElementById("map").style.cursor = "help";
+
+  }
+  popUp=()=>{
+    if(this.state.info==false){
+      this.setState({info:true})
+      console.log(this.state.info)
+    }else{
+      this.setState({info:false})
+      console.log(this.state.info)
+    }
+  }
+
+  render() {
+    var googleMarkers =this.state.markers.map((marker)=> {
+      return (
+           <InfoWindowMap lat={marker.latitude} lng={marker.longitude} key={marker.toilet_id} > </InfoWindowMap>
+        )  });
+      // const googleMarkers = this.state.markers.map(marker => (
+      //   <Marker
+      //   position={{ lat: parseFloat(marker.latitude), lng: parseFloat(marker.longitude) }}
+      //   key={marker.toilet_id}
+      //   onClick={this.popUp}
+      // >{this.state.info &&
+			// 	<InfoWindow
+			// 			onCloseClick={this.handleToggle}
+			// 			>
+			// 		<span>Something</span>
+			// 	</InfoWindow>
+			//  }
+      // </Marker>
+      // )); 
     
+    
+     
+
+    const Map = withGoogleMap(props => (
+      <GoogleMap
+        defaultCenter={{ lat: 60.171944, lng: 24.941389 }}
+        defaultZoom={16}
+        defaultOptions={{
+          streetViewControl: true,
+          scaleControl: true,
+          mapTypeControl: false,
+            panControl: true,
+            zoomControl: true,
+            rotateControl: true,
+            fullscreenControl: true,
+
+          }
+        }
+        
+        
+        onClick={this.getLocation} >
+        {googleMarkers}
       </GoogleMap>
-   ));
-   return(
-      <div>
-        <Map
-          containerElement={ <div style={{ height: `100vh`, width: '100Wh' }} /> }
-          mapElement={ <div style={{ height: `100%` }} /> }
-        />
+    ));
+    return (
+      <div id="map"   >
+        <button id="nappi" onClick={this.addToilet}>jeesus</button>
+        <Map id="map" 
+          containerElement={<div style={{ height: `100vh`, width: '100Wh' }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          controls={null}
+        >
+        </Map>
       </div>
-   );
-   }
+    );
+  }
 };
 export default Map;
